58dff70e93bb5481c4f53745ae54dc071aa50ff8
thomasad7 <91991727+thomasad7@users.noreply.github.com>
2022-02-20 20:32:57 +0000

    update

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    proxy: "localhost:8000",
+    proxy: "localhost:5130",
     files: ["**/*.css", "**/*.pug", "**/*.js"],
     ignore: ["node_modules"],
     reloadDelay: 10,
     ui: false,
     notify: false,
-    port: 3000,
+    port: 5180,
   };
-  
--- index.js
+++ index.js
@@ -7,12 +7,59 @@
  const express = require("express");
  const path = require("path");
 
+ const expressSession = require("express-session");
+ const passport = require("passport");
+ const Auth0Strategy = require("passport-auth0");
+ require("dotenv").config();
+
+ const authRouter = require("./auth");
+
 /**
  * App Variables
  */
 
  const app = express();
- const port = process.env.PORT || "8000";
+ const port = process.env.PORT || "5130";
+
+/**
+ * Session Configuration (New!)
+ */
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration (New!)
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
 
 /**
  *  App Configuration
@@ -21,6 +68,28 @@
  app.set("views", path.join(__dirname, "views"));
  app.set("view engine", "pug");
  app.use(express.static(path.join(__dirname, "public")));
+ app.use(expressSession(session));
+
+ passport.use(strategy);
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+ passport.serializeUser((user, done) => {
+  done(null, user);
+ });
+
+ passport.deserializeUser((user, done) => {
+  done(null, user);
+ });
+ 
+ // Creating custom middleware with Express
+ app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+ });
+
+ // Router mounting
+ app.use("/", authRouter);
 
 /**
  * Routes Definitions
